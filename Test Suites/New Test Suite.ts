95703cd02d201bc28ffb8fbb9415d5edfac6a6d3
bulinskim <bulinskim@CKHDHM2.gbcc.local>
2018-03-08 19:44:06 +0100

    upload project

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-08T19:10:47</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>22d4d868-6eb7-4f3b-a6a7-45c91bbdf4df</testSuiteGuid>
+   <testCaseLink>
+      <guid>cddfad52-17db-44be-b552-2943211507be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC1_Verify Successful Login</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>14d98e7b-4ac8-4e28-8ce7-394001d188f0</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a97344e7-5f9c-4936-92a2-d85e5463f3c9</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Open Close.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Open Close</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-08T19:32:59</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a80a0fa3-1f43-4996-b97b-edede026b3a9</testSuiteGuid>
+   <testCaseLink>
+      <guid>5241935c-a889-47bb-b10b-aef203d6ea36</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/OpenClose</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
