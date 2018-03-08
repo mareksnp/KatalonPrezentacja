2eb58fc56c192a0e3304b26094fe4af1fa4fb213
bulinskim <bulinskim@CKHDHM2.gbcc.local>
2018-03-08 20:44:02 +0100

    backup

---
+++ Test Suites/link.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>link</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-08T20:42:57</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a7fd6dc6-af80-4454-a7fb-f96e1f6f06e8</testSuiteGuid>
+   <testCaseLink>
+      <guid>fb845406-45df-421d-8893-444976f93777</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Backup/link</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
